11bebb8265b5318314c13dafa2ff33bbc9ebd57e
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-04-07 10:01:43 +0200

    Ajout visiteur condition

--- src/model/ConditionNode.model.ts
+++ src/model/ConditionNode.model.ts
@@ -3,10 +3,11 @@ import NodeModel from './Node.model'
 import VisiteurNode from './visiteurs/VisiteurNode'
 
 export default class ConditionNode extends NodeModel {
-  accept(visitor: VisiteurNode): void {
-    throw new Error('Method not implemented.')
-  }
   constructor(public value: ExpressionNode) {
     super()
+  }
+  accept(visitor: VisiteurNode): void {
+    visitor.visitExpression(this.value)
+    visitor.visitCondition(this)
   }
 }
