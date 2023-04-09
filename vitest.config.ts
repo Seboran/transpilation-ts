a5741cb63606121ad849b7112c5ef97f1181478f
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-04-09 09:38:30 +0200

    update coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: ['text', 'html', 'json-summary', 'json'],
+      provider: 'c8',
+    },
   },
 })
