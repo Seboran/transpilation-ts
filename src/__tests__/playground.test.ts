3c5aefb6772269649b2e4aa86705efe4d1410112
Nirina Rabeson <nirinarabeson@me.com>
2023-08-09 14:06:37 +0000

    ajout d'un test sur playground pour dormir sur les deux oreilles

---
+++ src/__tests__/playground.test.ts
@@ -0,0 +1,38 @@
+import { describe, test, expect } from 'vitest'
+import createMarkdown from '../createMarkdown'
+import {
+  AssignationNoeud,
+  LitteralNoeud,
+  AdditionNoeud,
+  NombreNoeud,
+} from '../model'
+import { JavascriptGenerator, CobolGenerator } from '../visiteurs'
+import prettier from 'prettier'
+
+describe('playground', () => {
+  test('should work', () => {
+    const instruction = new AssignationNoeud(
+      new LitteralNoeud('test'),
+      new AdditionNoeud(new NombreNoeud(5), new NombreNoeud(3))
+    )
+    const generateurJavascript = new JavascriptGenerator()
+    const prettiedCode = prettier.format(
+      instruction.accept(generateurJavascript),
+      {
+        parser: 'babel',
+      }
+    )
+    expect(prettiedCode).toEqual('var test = 5 + 3;\n')
+  })
+
+  test('other test', () => {
+    const instruction = new AssignationNoeud(
+      new LitteralNoeud('test'),
+      new AdditionNoeud(new NombreNoeud(5), new NombreNoeud(3))
+    )
+    const generateurCobol = new CobolGenerator()
+
+    const generatedCobol = instruction.accept(generateurCobol)
+    expect(generatedCobol).toEqual('ADD 5 3 GIVING test')
+  })
+})
