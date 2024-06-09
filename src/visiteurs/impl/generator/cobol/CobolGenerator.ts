8e0abde4fe05409db26b16f2988d66353c7c8e89
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-06-09 20:27:03 +0200

    ajout générateur cobol

--- src/visiteurs/impl/generator/cobol/CobolGenerator.ts
+++ src/visiteurs/impl/generator/cobol/CobolGenerator.ts
@@ -1,9 +1,57 @@
-import AbstractVisiteur from '../../AbstractVisiteur'
-import CobolOrchestrateur from '../../orchestrateur/cobol/CobolOrchestrateur'
+import AssignationNoeud from '../../../../model/AssignationNoeud.model'
+import ConditionNode from '../../../../model/ConditionNode.model'
+import ExpressionsNoeud from '../../../../model/ExpressionsNoeud.model'
+import LitteralNoeud from '../../../../model/LitteralNoeud.model'
+import NoeudModel from '../../../../model/Noeud.model'
+import NombreNoeud from '../../../../model/NombreNoeud.model'
+import SiNoeud from '../../../../model/SiNoeud.model'
+import SuperieurNoeud from '../../../../model/SuperieurNoeud.model'
+import VisiteurNoeud from '../../../VisiteurNoeud'
+import {
+  CobolAssignationVisiteur,
+  JsNombreVisiteur,
+  JsLitteralVisiteur,
+  JsConditionVisiteur,
+  JsSuperieurVisiteur,
+  AbstractVisiteurOrchestrateur,
+} from '../../orchestrateur'
+import CobolSiVisiteur from '../../orchestrateur/cobol/helpers/CobolSiVisiteur'
 
-export default class CobolGenerator extends AbstractVisiteur<string> {
+const visiteurMappings: Array<
+  [
+    new (...args: any[]) => NoeudModel,
+    new (...args: any[]) => VisiteurNoeud<string, NoeudModel>,
+  ]
+> = [
+  [AssignationNoeud, CobolAssignationVisiteur],
+  [NombreNoeud, JsNombreVisiteur],
+  [
+    ExpressionsNoeud,
+    class extends AbstractVisiteurOrchestrateur<string> {
+      visit(node: ExpressionsNoeud): string {
+        return node.expressions.map(super.visit.bind(this)).join('\n')
+      }
+    },
+  ],
+  [LitteralNoeud, JsLitteralVisiteur],
+  [SiNoeud, CobolSiVisiteur],
+  [ConditionNode, JsConditionVisiteur],
+  [SuperieurNoeud, JsSuperieurVisiteur],
+]
+
+export default class CobolOrchestrateur extends AbstractVisiteurOrchestrateur<string> {
   constructor() {
-    const orchestrateur = new CobolOrchestrateur()
-    super(orchestrateur.orchestre)
+    const orchestrateur: Record<string, VisiteurNoeud<string, NoeudModel>> = {}
+    super(orchestrateur)
+    visiteurMappings.forEach(([clazz, visiteurClass]) => {
+      this.add(clazz, new visiteurClass(orchestrateur))
+    })
+  }
+
+  add<T extends NoeudModel>(
+    clazz: new (...args: any[]) => T,
+    visiteur: VisiteurNoeud<string, T>,
+  ) {
+    this.orchestre[clazz.name] = visiteur
   }
 }
